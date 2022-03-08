a5a2f64b4312f6d108efb9be98567410fd87c2df
Zeeshan <Zeeshan1234802@gmail.com>
2022-03-08 23:03:30 +0530

    corrected

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -4,16 +4,13 @@ function favouriteMovie(operation, movie) {
     // Write your code here
     if(operation=='add'){
        movies.push(movie)
-       return movies
+       
     }
     else if(operation==='remove' && movies.length>0){
        movies.pop();
-       return movies
-    }
-    else{
-        return []
-    }
-
+       
+    }return movies
+  
     }
     
 
